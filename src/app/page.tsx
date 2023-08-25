3a5f6b934f73baea57584dd48b2987f580dd51c1
MatsBACCANO <43158600+Takashi-Matsumura@users.noreply.github.com>
2023-08-25 06:38:01 +0000

    1st commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Image from 'next/image'
+import { Button, ButtonGroup } from "@material-tailwind/react";
+
+export default function Home() {
+  return (
+    <div className='p-10'>
+      <h1>Hello World</h1>
+        <Button>Button</Button>
+    </div>
+  )
+}
